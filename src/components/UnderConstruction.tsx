24210937d8f92ba336b25c436b2d02e836c20b46
SauelAlmonte <almontesauel@gmail.com>
2025-09-26 22:11:39 -0400

    Added under constructyion overlay in certain sections

--- src/components/UnderConstruction.tsx
+++ src/components/UnderConstruction.tsx
@@ -4,10 +4,10 @@ import React from "react";
 
 const UnderConstruction = () => (
     <div
-        className="absolute inset-0 z-50 flex items-center justify-center bg-white/10 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-50 border border-cyan-400 max-w-7xl mx-auto"
+        className="isolate absolute inset-0 z-50 flex items-center justify-center bg-white/10 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50 border border-cyan-400 max-w-7xl mx-auto"
     >
-        <div className="px-8 py-4 rounded-xl bg-cyan-50 shadow-2xl border border-cyan-300">
-            <span className="text-2xl sm:text-3xl font-bold text-zinc-700">
+        <div className="px-8 py-4 rounded-2xl bg-cyan-50 shadow-2xl border-2 border-cyan-300 z-30 drop-shadow-2xl">
+            <span className="text-2xl sm:text-3xl font-bold text-zinc-700 z-50">
                 Under Construction
             </span>
         </div>
